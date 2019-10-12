a0c7dd04a82c4e21fed6c3827a96782131eefa78
Phaedrus <phraznikov@gmail.com>
2019-10-12 15:11:51 -0700

    Add tab navigation, favorites UI

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,15 +1,19 @@
+import React from 'react'
 import { Platform } from 'react-native'
 // import { createStackNavigator } from 'react-navigation'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
-// import { createTabsNavigator } from 'react-navigation-tabs'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 // import { createDrawerNavigator } from 'react-navigation-drawer'
+import { Ionicons } from '@expo/vector-icons'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 
 import Colors from '../constants/Colors'
 
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
+import FavoritesScreen from '../screens/FavoritesScreen'
 
 const MealsNavigator = createStackNavigator({
   Categories: {
@@ -29,4 +33,37 @@ const MealsNavigator = createStackNavigator({
   }
 })
 
-export default createAppContainer(MealsNavigator)
+const tabScreenConfig = {
+  Meals: {
+    screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
+        },
+        tabBarColor: Colors.primaryColor
+      }
+  },
+  Favorites: {
+    screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
+        },
+        tabBarColor: Colors.accentColor
+      }
+  }
+}
+
+const MealsFavTabNavigator =
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(tabScreenConfig, {
+        activeTintColor: 'white',
+        shifting: true
+      })
+    : createBottomTabNavigator(tabScreenConfig, {
+        tabBarOptions: {
+          activeTintColor: Colors.accentColor
+        }
+      })
+
+export default createAppContainer(MealsFavTabNavigator)
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,18 +1,40 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 
-import { CATEGORIES } from '../data/dummy-data'
+import { CATEGORIES, MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
 const CategoryMealsScreen = props => {
+  const renderMealItem = itemData => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          props.navigation.navigate({routeName: 'MealDetail', params: {
+            mealId: itemData.item.id
+          }})
+        }}
+      />
+    )
+  }
+
+  const catId = props.navigation.getParam('categoryId')
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  )
+
   return (
     <View style={styles.screen}>
-      <Text>The Category Meals Screen</Text>
-      <Button title="Go to Details" onPress={() => {
-        props.navigation.navigate("MealDetail")
-      }} />
-      <Button title="Go Back" onPress={() => {
-        props.navigation.pop()
-      }} />
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{width: '100%'}}
+      />
     </View>
   )
 }
@@ -20,7 +42,6 @@ const CategoryMealsScreen = props => {
 CategoryMealsScreen.navigationOptions = navigationData => {
   const catId = navigationData.navigation.getParam('categoryId')
   const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
-
   return {
     headerTitle: selectedCategory.title,
   }
